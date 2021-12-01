132faabad58c2cbf3732519fb74e31ca4843ba36
suyeon <rlatndus9911@naver.com>
2021-12-02 04:49:40 +0900

    Kingfisher 적용

--- Moya/DaangnMarket/DaangnMarket/Network/BaseAPI.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BaseAPI.swift
-//  DaangnMarket
-//
-//  Created by 김수연 on 2021/11/30.
-//
-
-import Foundation
---
+++ Moya/DaangnMarket/DaangnMarket/Network/GeneralAPI.swift
@@ -0,0 +1,14 @@
+//
+//  BaseAPI.swift
+//  DaangnMarket
+//
+//  Created by 김수연 on 2021/11/30.
+//
+
+import Foundation
+
+struct GeneralAPI {
+    static let baseURL = "https://asia-northeast3-daangnmarket-wesopt.cloudfunctions.net/api"
+}
+
+
